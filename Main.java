cdf04333ff66153312f34fbf99549ff9a4abd780
dbui1910 <68297071+dbui1910@users.noreply.github.com>
2022-04-29 04:42:30 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.nio.file.*;
-	import java.util.ArrayList;
-	import java.util.List;;
+import java.util.ArrayList;
+import java.util.List;
+
 	 
 	public class Main {
 	   
@@ -16,48 +17,49 @@ import java.nio.file.*;
 	    data = new String(Files.readAllBytes(Paths.get(fileName)));
 	    return data;
 	  }
-	  public static boolean wordBreak(String s, List<String> dictionary) {
-	        // create a dp table to store results of subproblems
-	        // value of dp[i] will be true if string s can be segmented
-	        // into dictionary words from 0 to i.
-	        boolean[] dp = new boolean[s.length() + 1];
-	     
-	        // dp[0] is true because an empty string can always be segmented.
-	        dp[0] = true;
-	     
-	        for(int i = 0; i <= s.length(); i++){
-	            for(int j = 0; j < i; j++){
-	                if(dp[j] && dictionary.contains(s.substring(j, i))){
-	                    dp[i] = true;
-	                    break;
-	                }
-	            }
+	  
+	  public static List<String> wordBreak(String s, List<String> dictionary) {
+		    
+	        List<String> afterBreak = new ArrayList();
+	        if (dictionary.contains(s))
+	        	afterBreak.add(s);
+	        for ( int i =1; i<= s.length(); i++) {
+	        	String left = s.substring(0,i);
+	        	if (dictionary.contains(left)) {
+	        		List <String> subList = wordBreak(s.substring(i), dictionary);
+	        		for (String word: subList) {
+	        			afterBreak.add( left + " "+ word);
+	        		}
+	        	}
 	        }
-	        return dp[s.length()];
+	        
+	        return afterBreak;
+	        
 	    }
 	  public static void main(String[] args) throws Exception
 	  {
 	    String data = readFileAsString("/Users/diembui/eclipse-workspace/Project3-AOA/src/input.txt");
 	    String dic = readOutputFileasString("/Users/diembui/eclipse-workspace/Project3-AOA/src/aliceInWonderlandDictionary.txt");
-	    
+	    String [] input= data.split("\n");
+	   
 	    String dictionary [] = dic.split("\n");
-	    /*for (int i = 0; i < dictionary.length; i++) {
-	        // Printing the elements of String array
-	        System.out.print(dictionary[i] + ", ");
-	    }*/
 	    List<String> dict = new ArrayList<>();
 	    for (String s : dictionary) {
 	    	dict.add(s);
 	    }
-	    if(wordBreak("aliceinwonderland", dict))
-	        System.out.print("Yes\n");
-	    else
-	        System.out.print("No\n");
-	    
-	  
-	  
-	  
-	}
+	    List<String> result= new ArrayList<String>();
+	    // I TRIED TO TAKE INPUT.TXT AND PRINT OUT ALL THE OUTPUTS BUT I GOT EMPTY STRINGS EXCEPT "TOUNGESTOES"
+	    /*for (String y : input) {
+	    	//System.out.println(y);
+	    	result = wordBreak(y, dict);
+	    	//System.out.println(y + "can be split into " + result);
+	    }
+	    for ( int i=0; i< input.length; i++) {
+	    	//result = wordBreak(input[i], dict);
+	    	//System.out.println(input[i] + "can be split into " + result);
+	    	
+	    }*/
+	    result = wordBreak("williamwillwritewonderfulwalrus", dict);
+	    System.out.println(result);
+	  }
 }
-
- 
